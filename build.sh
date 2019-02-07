ef6fe95b9f1f8e9191405f42d41eec42837aa2f1
izumimatsuo <gurux2daddy@gmail.com>
2019-02-07 23:47:53 +0900

    add compose

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker-compose up -d
+docker-compose exec runner bash -c "cd /root/build/tests &&\
+                                    ansible-playbook test.yml --syntax-check &&\
+                                    ansible-playbook test.yml &&\
+                                    py.test -v test.py --hosts="docker://localhost" &&\
+                                    docker rm -f localhost"
+docker-compose down
